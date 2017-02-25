0a9480e327ab18f6771765475fb9b3f0beb14330
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-02-25 22:12:09 +0200

    Initial commit

---
+++ ganomede.couchdb.replicator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+{% if index == "0" %}
+OTHER_INDEX="1"
+{% else %}
+OTHER_INDEX="0"
+{% endif %}
+
+DATABASES="
+{% for database in couchdb_databases %}
+  {{ database }}
+{% endfor %}
+"
+for DB_NAME in $DATABASES; do
+  echo "New replication:"
+cat << EOF > tmp-replicator.json
+{
+"_id": "replicate-$DB_NAME-$OTHER_INDEX-to-{{index}}",
+"create_target": true,
+"continuous": true,
+"source": "http://couchdb-$OTHER_INDEX.{{dns_domain}}:{{config.couchdb.port}}/$DB_NAME",
+"target": "http://couchdb-{{index}}.{{dns_domain}}:{{config.couchdb.port}}/$DB_NAME"
+}
+EOF
+
+  cat tmp-replicator.json
+  #sleep 1
+  #echo 3
+  #sleep 1
+  #echo 2
+  #sleep 1
+  #echo 1
+  #sleep 1
+  #echo
+  curl -H "Content-type: application/json" http://couchdb-{{index}}.{{dns_domain}}:{{config.couchdb.port}}/_replicator -d @tmp-replicator.json
+  echo
+done
